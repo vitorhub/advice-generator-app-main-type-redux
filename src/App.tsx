aa48639a6e4a5936938dc9e250707019bf66561a
Vitor Falcao <joaofalcao.33@outlook.com>
2023-03-09 15:07:42 -0300

    advice generator frontendmentor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Dice from './images/icon-dice.png';
+import LinhaDesk from './images/pattern-divider-desktop.svg'
+import LinhaMobile from './images/pattern-divider-mobile.svg'
 
-function App() {
+const App: React.FC = () => {
+  const [estadoId, setEstadoId] = useState(0)
+  const [estadoAdvice, setEstadoAdvice] = useState("None Advice")
+
+  function buscaAdvice() {
+    fetch(`https://api.adviceslip.com/advice`) // para fetch
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setEstadoId(data.slip.id);
+        setEstadoAdvice(data.slip.advice)
+      })
+      .catch((err) => console.log(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <p className='adviceId'>Advice # {estadoId}</p>
+        <p className='adviceName'>"{estadoAdvice}"</p>
+        <img className='divider-desk' src={LinhaDesk} alt="linha" /><br/>
+        <img className='divider-mobile' src={LinhaMobile} alt="linha" /><br/>
+        <button className='botao' onClick={buscaAdvice}> <img className='dice' src={Dice} /> </button>
+      </div>
+      <Footer />
     </div>
   );
 }
